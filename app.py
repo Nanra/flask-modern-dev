149f05c7d1ad180ecbffa2b9ba215788dcb9a8e4
Nanra <sukedynanra@gmail.com>
2019-08-08 00:37:03 +0700

    :hammer: Iterable in Flask Render Themplate :hammer:

--- app.py
+++ app.py
@@ -2,8 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+## Variable Zone
+listProgrammingLang = ['Python', 'Java', 'C', 'JavaScript']
+webTitle = 'Flask Modern Web Development'
+title = "Superdeveloper"
+
+
 @app.route('/')
 def hello_world():
-    title = "Superdeveloper"
-    return render_template('index.html', title=title)
-
+    
+    return render_template('index.html',
+    header=title,
+    listBahasaPemrograman=listProgrammingLang,
+    judulWebsite=webTitle)
